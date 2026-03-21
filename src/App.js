f26ee49f22178b8a611d8c052080e9ea84db629d
jaqbek.eth <jaqbek.eth@gmail.com>
2026-03-21 13:41:57 +0100

    Create src/App.js file for main React component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Calendar and Todo App</h1>
+      {/* Your Calendar Component will go here */}
+      {/* Your Todo Component will go here */}
+    </div>
+  );
+};
+
+export default App;
